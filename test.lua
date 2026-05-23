b01825c45ab151cd7f204eaabbfca1f3b4b54304
ccrrrr7 <i7424446@gmail.com>
2026-05-23 09:27:45 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,18 @@
+Library.Localization:AddStrings("zh", {
+    ["Aimbot"] = "自瞄",
+    ["ESP Box"] = "ESP方框",
+    ["Hit Sound"] = "命中音效",
+})
+
+Library.Localization:AddStrings("ko", {
+    ["Aimbot"] = "에임봇",
+    ["ESP Box"] = "ESP 박스",
+})
+
+Library.Localization:AddStrings("es", {
+    ["Aimbot"] = "Puntería automática",
+})
+
+Library.Localization:AddStrings("ru", {
+    ["Aimbot"] = "Аимбот",
+})
